bf89bcb8fb4a476efc7bca0fd750e039254c5c44
kubarada <jakub.rd@seznam.cz>
2023-09-20 10:40:49 +0200

    adding new methods and adjusting main code

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ import cv2
 INPUT = 'data/frame_027673.png'
 OUTPUT = 'data/output/out1.png'
 
+coordinates = [(843, 477), (841, 480), (842, 481), (843, 483), (845, 485), (846, 488), (845, 490), (844, 492), (845, 495), (845, 498), (844, 500), (843, 503), (843, 506), (842, 508), (842, 511), (841, 514), (840, 517), (840, 520), (839, 523), (838, 526)]
+
 frame = cv2.imread(INPUT)
+
+frame = methods.draw_trajectory(frame, coordinates)
 
 if methods.is_daytime(frame):
      cv2.putText(frame, "is_day=1", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
